6e719f554e41b7a68f6eaee430b3607119cecac4
mas0061 <mas0061@gmail.com>
2013-12-20 11:10:52 +0900

    IntelliJ IDEAのプロジェクトとした

---
+++ src/main/java/net/mas0061/astah/plugin/classes2excel/Activator.java
@@ -0,0 +1,15 @@
+package net.mas0061.astah.plugin.classes2excel;
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext context) {
+	}
+
+	public void stop(BundleContext context) {
+	}
+	
+}
