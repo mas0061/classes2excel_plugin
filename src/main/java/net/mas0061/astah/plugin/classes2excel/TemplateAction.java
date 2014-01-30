09e664bc298434c72a5b74ccb3bc5de46b8bb854
mas0061 <mas0061@gmail.com>
2014-01-31 08:21:45 +0900

    一旦プラグイン完成

--- src/main/java/net/mas0061/astah/plugin/classes2excel/TemplateAction.java
+++ src/main/java/net/mas0061/astah/plugin/classes2excel/TemplateAction.java
@@ -7,30 +7,50 @@ import com.change_vision.jude.api.inf.model.IModel;
 import com.change_vision.jude.api.inf.project.ProjectAccessor;
 import com.change_vision.jude.api.inf.ui.IPluginActionDelegate;
 import com.change_vision.jude.api.inf.ui.IWindow;
-import net.mas0061.astah.plugin.classes2excel.read.ElementWithAnnotation;
+import com.sun.codemodel.internal.JOp;import net.mas0061.astah.plugin.classes2excel.read.ElementWithAnnotation;
 import net.mas0061.astah.plugin.classes2excel.read.FileExporter;
 import net.mas0061.astah.plugin.classes2excel.read.ReadClasses;
 
-import javax.swing.*;
+import javax.swing.*;import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 import java.util.List;
 
 public class TemplateAction implements IPluginActionDelegate {
-    private IWindow currentWindow;
 
 	public Object run(IWindow window) throws UnExpectedException {
 	    try {
-	        currentWindow = window;
 	        AstahAPI api = AstahAPI.getAstahAPI();
 	        ProjectAccessor projectAccessor = api.getProjectAccessor();
 	        IModel project = projectAccessor.getProject();
 
+	        JFileChooser chooser = new JFileChooser();
+	        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+	        chooser.setMultiSelectionEnabled(false);
+	        chooser.setFileFilter(new FileNameExtensionFilter("Excelファイル(*.xlsx)", "xlsx"));
+
+	        int selected = chooser.showSaveDialog(window.getParent());
+
+	        if (selected == JFileChooser.CANCEL_OPTION) {
+			    JOptionPane.showMessageDialog(window.getParent(), "ファイルを選択して下さい。", "Warning", JOptionPane.WARNING_MESSAGE);
+			    return null;
+            }
+
+            File saveFile = chooser.getSelectedFile();
+            
+            if (saveFile.exists()) {
+                int overWrite = JOptionPane.showConfirmDialog(window.getParent(), "既にファイルが存在します。上書きしますか？",
+                    "ファイル上書き確認", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                if (overWrite == JOptionPane.NO_OPTION) {
+                    return null;
+                }
+            }
+
             ReadClasses reader = new ReadClasses(projectAccessor, project);
             List<ElementWithAnnotation> classList = reader.getClassStructure();
             List<ElementWithAnnotation> attrList = reader.getClassesAttributes();
-            String outFileName = System.getProperty("user.home") + "/Desktop/outAll.xlsx";
-            new FileExporter().exportAllListExcel(classList, attrList, outFileName);
+            new FileExporter().exportAllListExcel(classList, attrList, saveFile);
 
-	        JOptionPane.showMessageDialog(currentWindow.getParent(), "Excelエクスポート終了");
+	        JOptionPane.showMessageDialog(window.getParent(), "Excelファイルへのエクスポートが正常に完了しました。");
 	    } catch (ProjectNotFoundException e) {
 	        String message = "Project is not opened.Please open the project or create new project.";
 			JOptionPane.showMessageDialog(window.getParent(), message, "Warning", JOptionPane.WARNING_MESSAGE); 
